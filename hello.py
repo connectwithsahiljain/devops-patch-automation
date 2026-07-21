989166ef050011252e034647ec41e09a04973a31
connectwithsahiljain <connectwithsahiljain@gmail.com>
2026-07-21 11:04:07 +0530

    hello

--- hello.py
+++ hello.py
@@ -1,1 +1,2 @@
 print("sahil is a cloud engineer with 5 years of experience, currently working at Deloitte in Bangalore. He specializes in AWS and is passionate about Docker.")
+print("Version request :" , requests.__version__)
